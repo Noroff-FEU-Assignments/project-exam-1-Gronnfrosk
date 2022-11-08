efc92d27d81ae140e86aed55b7721b78e570f2a6
Gronnfrosk <Hanna.Fjeldsaa@stud.noroff.no>
2022-11-08 16:00:02 +0100

    WordPress content except contact page

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,30 @@
+// home page cover
+const header = document.querySelector(".header");
+const headerImage = document.querySelector(".image_header");
+const slogan = document.querySelector(".slogan_title");
+const headerUrl = "https://gronnfrosk.one/project/wp-json/wc/store/products?per_page=20";
+
+async function renderHeader(headerUrl) {
+	const response = await fetch(headerUrl);
+	const content = await response.json();
+
+	header.innerHTML = `
+    <h1>${content[16].description}</h1>
+    `;
+	headerImage.innerHTML = `
+    <img src="${content[16].images[0].src}" alt="The golden retriever Goldy standing on stones" class="home_page" />
+    `;
+	slogan.innerHTML = `
+    <h2>${content[16].attributes[0].terms[0].name}</h2>
+    `;
+}
+renderHeader(headerUrl);
+
+// slideshow list
 const containerLatest = document.querySelector(".latest_post");
+const slideContainer = document.querySelector("#latest_post");
+const prevButton = document.querySelector(".swiper-button-prev");
+const nextButton = document.querySelector(".swiper-button-next");
 const url = "https://gronnfrosk.one/project/wp-json/wc/store/products";
 
 async function renderBlogs(url) {
@@ -19,11 +45,6 @@ async function renderBlogs(url) {
 }
 renderBlogs(url);
 
-// slideshow list
-const slideContainer = document.querySelector("#latest_post");
-const prevButton = document.querySelector(".swiper-button-prev");
-const nextButton = document.querySelector(".swiper-button-next");
-
 prevButton.onclick = () => {
 	slideContainer.scrollLeft -= 290;
 };
@@ -42,9 +63,9 @@ async function renderFavorite(otherUrl) {
 
 	fav.innerHTML += `
 		    <div class="favorites-container">    
-                <img src="${favorites[4].source_url}" alt="Goldy's favorite toy bone">
-                <img src="${favorites[5].source_url}" alt="Goldy's favorite snack, called fresh kisses">
-                <img src="${favorites[6].source_url}" alt="Goldy's best dog friend">
+                <img src="${favorites[5].source_url}" alt="Goldy's favorite toy bone">
+                <img src="${favorites[6].source_url}" alt="Goldy's favorite snack, called fresh kisses">
+                <img src="${favorites[7].source_url}" alt="Goldy's best dog friend">
             </div>`;
 }
 renderFavorite(otherUrl);
